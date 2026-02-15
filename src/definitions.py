e6d40565a03495728223325d14798a5d27387e98
bvntsev <208866670+bvntsev@users.noreply.github.com>
2026-02-15 13:55:52 +0300

    Update. Change structure file, this project waiting near big update (change all system for more user-friendly experience. Slope to cli)

---
+++ src/definitions.py
@@ -0,0 +1,98 @@
+org_schedule = [
+    # DENOMINATOR
+    [
+        # MONDAY
+        '''** 0. Computer Networks [Lec]    4231
+** 6. Modern Art [Sem]           3124
+''',
+        # TUESDAY
+        '''** 1. Thermodynamics [Lec]       1342
+** 2. Linear Programming [Sem]   4153
+** 3. Creative Writing [Sem]     3011
+** 4. Robotics [Lab]             4420
+** 5. Philosophy of Mind [Lec]   4007
+** 6. Mechanical Design [Cons]   3508
+''',
+        # WEDNESDAY
+        '''** 1. Machine Learning [Sem]     3225
+** 2. PE [Sem]                   5120
+** 3. Quantum Physics [Sem]      3333
+** 4. Data Structures [Lec]      1212
+** 6. Programming in C [Cons]    3246
+''',
+        # THURSDAY
+        '''** 1. Algorithms [Sem]           4113
+** 2. Circuit Analysis [Lab]     3348
+** 3. Neural Networks [Sem]      3107
+** 4. Calculus [Lec]             1208
+** 5. Ethics in AI [Cons]        3419
+** 6. Digital Systems [Cons]     3264
+** 6. Cybersecurity [Sem]        4117
+''',
+        # FRIDAY
+        '''** 1. History of Science [Lec]   1221
+** 2. PE [Sem]                   5108
+** 3. Philosophy [Sem]           3213
+''',
+        # SATURDAY
+        '''** 4. Programming in C [Cons]    4142
+        ''',
+        # SUNDAY
+        '''** ...'''
+    ],
+    # NUMERATOR
+    [
+        # MONDAY
+        '''** 0. Discrete Math [Cons]       3247
+** 2. Database Systems [Lec]     1204
+** 3. Embedded Systems [Sem]     4108
+** 4. Cyber Law [Cons]           4208
+** 6. Linguistics [Cons]         3330
+''',
+        # TUESDAY
+        '''** 1. Thermodynamics [Lec]       1210
+** 2. Database Systems [Lab]     3134
+** 3. Database Systems [Lab]     3134
+** 4. Sociology [Sem]            3310
+''',
+        # WEDNESDAY
+        '''** 2. PE [Sem]                   5102
+** 3. Statistics [Sem]           3341
+** 4. Computer Graphics [Lec]    1205
+** 5. Artificial Intelligence [Cons] 3218
+** 6. Data Analysis [Cons]       3221
+''',
+        # THURSDAY
+        '''** 2. Linear Algebra [Sem]       3235
+** 3. Quantum Computing [Sem]    3312
+** 4. Probability Theory [Lec]   1213
+** 5. English [Sem]              3115
+** 6. Machine Learning [Cons]    3245
+''',
+        # FRIDAY
+        '''** 1. Ethics [Lec]               1207
+** 2. PE [Sem]                   5111
+** 3. Software Engineering [Lec] 1209
+** 4. Web Development [Lec]      1211
+''',
+        # SATURDAY
+        '''** 4. Data Science [Cons]        4110
+        ''',
+        # SUNDAY
+        '''** ...
+        '''
+    ]
+]
+
+# Your a time schedule. Check README important
+time_schedule = dict({
+    0: "08:00",
+    1: "09:00",
+    2: "10:30",
+    3: "12:00",
+    4: "14:00",
+    5: "15:30",
+    6: "17:00",
+    7: "18:30",
+    8: "20:00"
+})
--- src/org-yeargen.py
+++ src/org-yeargen.py
@@ -61,92 +61,92 @@ def mkdir(path: str):
         exit(-1)
 
 
-if len(sys.argv) != 3:
-    print("Incorrect argument.\'mk_year_org {path_to_dir} {year}\'")
-    sys.exit(-1)
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Incorrect argument.\'python org-yeargen.py {path_to_dir} {year}\'")
+        sys.exit(-1)
 
 
-user_path = sys.argv[1]
-user_path = user_path[:-1] if user_path[-1] == '/' else user_path
-user_year = sys.argv[2]
-user_year_i = int(user_year)
+    user_path = sys.argv[1]
+    user_path = user_path[:-1] if user_path[-1] == '/' else user_path
+    user_year = sys.argv[2]
+    user_year_int = int(user_year)
 
 
-if not user_year.isdecimal() or \
-    not (datetime.MINYEAR <= user_year_i <= datetime.MAXYEAR):
-    print("Incorrect a year number {only decimal}")
-    print(f'Correct year interval is [{datetime.MINYEAR}, {datetime.MAXYEAR}]')
-    exit(-1)
+    if not user_year.isdecimal() or \
+        not (datetime.MINYEAR <= user_year_int <= datetime.MAXYEAR):
+        print("Incorrect a year number {only decimal}")
+        print(f'Correct year interval is [{datetime.MINYEAR}, {datetime.MAXYEAR}]')
+        exit(-1)
 
-path = f'{user_path}/{user_year}/'
+    path = f'{user_path}/{user_year}/'
 
-'''
-Example current day:
-(f'{user_path}/{user_year}/%m_%B/{weekN}W_%d_%m_%A_{user_year}.org')
-'''
-print("======PATH======")
-print(path)
+    '''
+    Example current day:
+    (f'{user_path}/{user_year}/%m_%B/{weekN}W_%d_%m_%A_{user_year}.org')
+    '''
+    print("======PATH======")
+    print(path)
 
 
-print("====MK_YEAR====")
+    print("====MK_YEAR====")
 
-if not ck_exist_path(path):
-    print("LOG: Year folder isn't exist, make new dir")
-    mkdir(path)
-else:
-    print("LOG: Year folder already exist")
+    if not ck_exist_path(path):
+        print("LOG: Year folder isn't exist, make new dir")
+        mkdir(path)
+    else:
+        print("LOG: Year folder already exist")
 
 # date_data = datetime.datetime(int(user_year))
 # print(type(date_data))
 
-first_study_day = datetime.datetime(user_year_i, 9, 1)
+    first_study_day = datetime.datetime(user_year_int, 2, 2)
 
+    print("====MK_MONTHS====")
+    for iter_month in range(1, 12+1):
+        curr_month = months[iter_month]
 
-print("====MK_MONTHS====")
-for iter_month in range(1, 12+1):
-    curr_month = months[iter_month]
+        # date_data.month = curr_month
 
-    # date_data.month = curr_month
+        print(f'---{curr_month}')
+        path_temp_month = path + \
+            f'{(str(iter_month) if iter_month > 9 else '0' + str(iter_month))}' + \
+            '_' + f'{curr_month}/'
 
-    print(f'---{curr_month}')
-    path_temp_month = path + \
-        f'{(str(iter_month) if iter_month > 9 else '0' + str(iter_month))}' + \
-        '_' + f'{curr_month}/'
-
-    if not(ck_exist_path(path_temp_month)):
-        print("LOG:", curr_month, "isn't exist, make new dir")
-        mkdir(path_temp_month)
-    else: 
-        print("LOG:", curr_month, "folder is already exist")
-    
-    num_created_files = 0
-    for \
-    iter_day in range(1, calendar.monthrange(user_year_i, iter_month)[1] + 1):
-
-        curr_day = datetime.datetime(user_year_i, iter_month, iter_day)
-
-        num_week = ((abs(first_study_day - curr_day)).days // 7 + 1) 
-
-        ISO_week_n = datetime.date.\
-        fromisoformat(curr_day.isoformat()[:curr_day.isoformat().find('T')]).\
-        isocalendar().week
+        if not(ck_exist_path(path_temp_month)):
+            print("LOG:", curr_month, "isn't exist, make new dir")
+            mkdir(path_temp_month)
+        else: 
+            print("LOG:", curr_month, "folder is already exist")
         
-        path_temp_day = \
-        path_temp_month + f'{ISO_week_n}' + \
-            f'W_{str(iter_day) if iter_day > 9 else '0' + str(iter_day)}'+\
-            f'_{str(iter_month) if iter_month > 9 else '0' + str(iter_month)}'+\
-            '_' + f'{day_names[curr_day.isoweekday()]}.org'
+        num_created_files = 0
+        for \
+        iter_day in range(1, calendar.monthrange(user_year_int, iter_month)[1] + 1):
 
-        # print(path_temp_day)
+            curr_day = datetime.datetime(user_year_int, iter_month, iter_day)
 
-        if (ck_exist_path(path_temp_day)):
-            continue
-        file = open(path_temp_day, 'w')
+            num_week = ((abs(first_study_day - curr_day)).days // 7 + 1) 
 
-        org_schedule = get_day_content(curr_day, num_week)
-        with open(path_temp_day, 'w') as file:
-            file.write(org_schedule)
-        
+            ISO_week_n = datetime.date.\
+            fromisoformat(curr_day.isoformat()[:curr_day.isoformat().find('T')]).\
+            isocalendar().week
+            
+            path_temp_day = \
+            path_temp_month + f'{ISO_week_n}' + \
+                f'W_{str(iter_day) if iter_day > 9 else '0' + str(iter_day)}'+\
+                f'_{str(iter_month) if iter_month > 9 else '0' + str(iter_month)}'+\
+                '_' + f'{day_names[curr_day.isoweekday()]}.org'
 
-        num_created_files += 1
-    print(f'{curr_month}: Created the {num_created_files} new files')
+            # print(path_temp_day)
+
+            if (ck_exist_path(path_temp_day)):
+                continue
+            file = open(path_temp_day, 'w')
+
+            org_schedule = get_day_content(curr_day, num_week)
+            with open(path_temp_day, 'w') as file:
+                file.write(org_schedule)
+            
+
+            num_created_files += 1
+        print(f'{curr_month}: Created the {num_created_files} new files')
